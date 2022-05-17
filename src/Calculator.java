779a28f860172170781d84877adf95ff72f528f4
kchs94 <kchs94@gmail.com>
2022-05-17 20:29:54 +0900

    계산기 기본 기능 구현 완료

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,21 +5,34 @@ public class Calculator {
     Scanner in = new Scanner(System.in);
     System.out.print("첫 번째 값 입력: ");
     int first = in.nextInt();
-    System.out.print("두 번째 값 입력: ");
-    int second = in.nextInt();
-    System.out.print("사칙연산 +,-,*,/ 중 하나를 입력하세요: ");
-    String op = in.next();
 
-    if (op.equals("+"))
-      System.out.println("덧셈: " + (first + second));
-    else if(op.equals("-"))
-      System.out.println("뺄셈: " + (first - second));
-    else if(op.equals("*"))
-      System.out.println("곱셈: " + (first * second));
-    else if(op.equals("/"))
-      System.out.println("나눗셈: " + (first / second));
-    else
-      System.out.println("사칙연산 기호가 아닙니다.");
+    int result = first;
+    while (true) {
+      System.out.print("사칙연산 +,-,*,/ 중 하나를 입력하세요: ");
+      String op = in.next();
 
+      if (op.equals("quit")) {
+        System.out.println("프로그램을 종료합니다.");
+        break;
+      }
+
+      System.out.print("두 번째 값 입력: ");
+      int second = in.nextInt();
+
+      if (op.equals("+")){
+        result = result + second;
+        System.out.println("덧셈: " + result);
+      } else if(op.equals("-")){
+        result = first - second;
+        System.out.println("뺄셈: " + (first - second));
+      } else if(op.equals("*")) {
+        result = first * second;
+        System.out.println("곱셈: " + (first * second));
+      } else if(op.equals("/")) {
+        result = first / second;
+        System.out.println("나눗셈: " + (first / second));
+      } else
+        System.out.println("사칙연산 기호가 아닙니다.");
+    }
   }
 }
