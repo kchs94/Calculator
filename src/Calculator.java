5a2c8ceded1064ac015f0ee95ee84d298a8c68f8
kchs94 <kchs94@gmail.com>
2022-05-17 23:13:35 +0900

    메소드를 추가해 로직을 분리

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,38 +1,63 @@
 import java.util.Scanner;
 
 public class Calculator {
-  public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
+  static int getFirstValue(Scanner scanner) {
     System.out.print("첫 번째 값 입력: ");
-    int first = in.nextInt();
+    int first = scanner.nextInt();
+    return first;
+  }
+
+  static String getSymbol(Scanner scanner) {
+    System.out.print("사칙연산 +,-,*,/ 중 하나를 입력하세요: ");
+    String op = scanner.next();
+    return op;
+  }
+
+  static int getSecondValue(Scanner scanner) {
+    System.out.print("두 번째 값 입력: ");
+    int second = scanner.nextInt();
+    return second;
+  }
+
+  static int calculate(int first, String operator, int second) {
+    int result = 0;
+    if (operator.equals("+")){
+      result = first + second;
+      System.out.println("덧셈: " + result);
+    } else if(operator.equals("-")){
+      result = first - second;
+      System.out.println("뺄셈: " + result);
+    } else if(operator.equals("*")) {
+      result = first * second;
+      System.out.println("곱셈: " + result);
+    } else if(operator.equals("/")) {
+      result = first / second;
+      System.out.println("나눗셈: " + result);
+    } else
+      System.out.println("사칙연산 기호가 아닙니다.");
+    return result;
+  }
+
+  static void print(int result) {
+    System.out.println("최종 결과 값: " + result);
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int first = getFirstValue(scanner);
 
     int result = first;
     while (true) {
-      System.out.print("사칙연산 +,-,*,/ 중 하나를 입력하세요: ");
-      String op = in.next();
+      String operator = getSymbol(scanner);
 
-      if (op.equals("quit")) {
-        System.out.println("프로그램을 종료합니다.");
+      if (operator.equals("quit")) {
+        print(result) ;
         break;
       }
+      int second = getSecondValue(scanner);
 
-      System.out.print("두 번째 값 입력: ");
-      int second = in.nextInt();
-
-      if (op.equals("+")){
-        result = result + second;
-        System.out.println("덧셈: " + result);
-      } else if(op.equals("-")){
-        result = first - second;
-        System.out.println("뺄셈: " + (first - second));
-      } else if(op.equals("*")) {
-        result = first * second;
-        System.out.println("곱셈: " + (first * second));
-      } else if(op.equals("/")) {
-        result = first / second;
-        System.out.println("나눗셈: " + (first / second));
-      } else
-        System.out.println("사칙연산 기호가 아닙니다.");
+      result = calculate(result, operator, second);
     }
   }
 }
